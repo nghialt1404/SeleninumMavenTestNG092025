fd23f839a81cc7e5aada4cc82ed3340f63cfda7e
Rayairfeed <nghialt1404@gmail.com>
2025-10-22 23:28:20 +0700

    Add gitignore

---
+++ src/test/java/TestNg/RunTestNG.java
@@ -0,0 +1,51 @@
+package TestNg;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
+
+import java.time.Duration;
+
+public class RunTestNG {
+    WebDriver driver;
+
+    @BeforeMethod
+    public void createDriver() {
+        driver = new ChromeDriver();
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+        driver.manage().timeouts().pageLoadTimeout(Duration.ofSeconds(30));
+    }
+
+    @Test
+    public void testAnhTesterBlog() throws InterruptedException {
+        driver.get("https://anhtester.com");
+        driver.findElement(By.xpath("//a[normalize-space()='blog']")).click();
+        Thread.sleep(2000);
+
+        WebElement element = driver.findElement(By.xpath("//h2[@class='card__title mt-0']//a[contains(text(),'So sánh Playwright JS/TS và Playwright Python')]"));
+        Assert.assertEquals(element.getText(), "So sánh Playwright JS/TS và Playwright Python");
+    }
+
+
+    @Test
+    public void testGoogleSearch() throws InterruptedException {
+        driver.get("https://www.google.com/");
+        driver.findElement(By.xpath("//textarea[@name='q']")).sendKeys("anhtester", Keys.ENTER);
+        Thread.sleep(2000);
+        driver.findElement(By.xpath("//h3[normalize-space()='Anh Tester Automation Testing']")).click();
+        Thread.sleep(2000);
+    }
+
+    @AfterMethod
+    public void closeDriver() {
+        driver.quit();
+    }
+}
